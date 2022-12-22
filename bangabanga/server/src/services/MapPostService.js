56d7c0bb5325e5c4a5cd70a8e41ee9380bff4b7e
kkssbbb <03337@naver.com>
2022-12-22 22:24:01 +0900

    ♻️refactor(): 지도로 보기에서 지역명(홍대)으로 get요청 api 객체에 recruitingNum가 0일떄 `현재 ${locationDetail} 지역은 모집중인 카페가 없습니다`메세지 전달

--- bangabanga/server/src/services/MapPostService.js
+++ bangabanga/server/src/services/MapPostService.js
@@ -11,7 +11,7 @@ class MapPostService {
 
   //지도로 보기에서 지역명(홍대)으로 get요청 api
   async getLocationfilterPosts(locationDetail) {
-    let recruitingNum = 0; //현재모집중인
+
 
     //카페정보테이블 ,모집글 조회해서
     //조건1 필터링하기
@@ -34,9 +34,12 @@ class MapPostService {
     const matchingPosts = await sequelize.query(query, {
       type: QueryTypes.SELECT,
     });
+    console.log(matchingPosts[0].recruitingNum);
+    
+    if(matchingPosts[0].recruitingNum === 0){
+     return   `현재 ${locationDetail} 지역은 모집중인 카페가 없습니다`
+    }
 
-
-    recruitingNum = matchingPosts.length;
 
     return matchingPosts;
   }
